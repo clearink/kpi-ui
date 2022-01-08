d2df23177537d012a05a030be1d24d077cff9f10
clearink <1561939638@qq.com>
2022-01-09 01:14:36 +0800

    1

--- packages/kpi-ui/lib/button/use_ripple.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = useRipple;
-
-function useRipple(props) {}
--- packages/kpi-ui/types/button/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import { ButtonProps } from './props';
-declare function Button(props: ButtonProps): JSX.Element;
-export default Button;
