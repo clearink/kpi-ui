ee7aeebf50c82d5474c1bc0903b8550f05545769
clearink <1561939638@qq.com>
2022-08-16 20:39:00 +0800

    123

--- packages/kpi-ui/src/_utils/with_default_props.ts
+++ packages/kpi-ui/src/_utils/with_default_props.ts
@@ -3,7 +3,11 @@ import { ComponentType } from 'react'
 type UnionProp<P, D> = {
   [K in keyof D]+?: K extends keyof P ? P[K] : D[K]
 } & Omit<P, keyof D>
-export default function withDefaultProps<P extends object, D extends Partial<P> = Partial<P>>(
+type SomeDefault<T extends object> = Readonly<Partial<T>>
+export default function withDefaultProps<
+  P extends object,
+  D extends SomeDefault<P> = SomeDefault<P>
+>(
   WrappedComponent: ComponentType<P>,
   defaultProps?: D // 推荐 as const 使用, 避免多生成类型
 ) {
