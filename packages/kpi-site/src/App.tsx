12505233127fd65616d1c211c69702fabea03253
clearink <1561939638@qq.com>
2023-02-24 14:43:04 +0800

    123

--- packages/kpi-site/src/App.tsx
+++ packages/kpi-site/src/App.tsx
@@ -1,12 +1,16 @@
-import { Form, Button } from '@kpi/ui'
+import { Form, Button, Space } from '@kpi/ui'
 import kv from '@kpi/validate'
-import { useEffect, useLayoutEffect, useReducer } from 'react'
+import { useEffect, useLayoutEffect, useReducer, useState } from 'react'
 
 function Input(props: any) {
   return <input {...props} value={props.value || ''} style={{ height: 32 }} />
 }
 
 export default function App() {
+  const [noStyle, setNoStyle] = useState(true)
+  const [noRule, setNoRule] = useState(true)
+  const [inputNumber, setInputNumber] = useState(3000)
+
   const start = performance.now()
   setTimeout(() => {
     console.log('diff:setTimeout', performance.now() - start)
@@ -17,26 +21,34 @@ export default function App() {
   useEffect(() => {
     console.log('diff:useEffect', performance.now() - start)
   }, [start])
-  const forceUpdate = useReducer((c) => c + 1, 0)[1]
+
   return (
     <div>
-      <Button onClick={() => forceUpdate()}>forceUpdate</Button>
-      <Form as="div">
-        <Form.Item name="a" initialValue="a1">
-          <Input />
-        </Form.Item>
-        <Form.Item name="a" initialValue="a2">
-          <Input />
-        </Form.Item>
-        <Form.List name="username" initialValue={Array(3000).fill(1)}>
-          {(fields) => {
-            return fields.map((field) => (
-              <Form.Item label="123123" {...field} rule={kv.string().required()}>
-                <Input placeholder={`name-${field.name}`} />
-              </Form.Item>
-            ))
-          }}
-        </Form.List>
+      <p style={{ fontSize: 20, textAlign: 'center', marginBottom: 20 }}>
+        测试 3000 个输入框常见下 Form 组件的性能
+      </p>
+      <Space style={{ display: 'flex', justifyContent: 'center', marginBottom: 20 }}>
+        <Button type="primary" onClick={() => setNoStyle((p) => !p)}>
+          NoStyle: {noStyle ? 'true' : 'false'}
+        </Button>
+        <Button type="primary" onClick={() => setNoRule((p) => !p)}>
+          NoRule: {noStyle ? 'true' : 'false'}
+        </Button>
+        <span>input number</span>
+        <Input value={inputNumber} onChange={(e) => setInputNumber(parseInt(e.target.value, 10))} />
+      </Space>
+      <Form as="div" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        {Array.from({ length: inputNumber }, (_, i) => (
+          <Form.Item
+            key={i}
+            label="123123"
+            noStyle={noStyle}
+            name={['username', i]}
+            rule={noRule ? undefined : kv.string().required()}
+          >
+            <Input placeholder={`username-${i}`} />
+          </Form.Item>
+        ))}
       </Form>
     </div>
   )
