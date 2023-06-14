a0e5e2333dc0d538dd045095d17f5f3b3f430ae9
clearink <1561939638@qq.com>
2023-06-14 22:44:58 +0800

    123

--- packages/kpi-site/src/motion/easing/index.ts
+++ packages/kpi-site/src/motion/easing/index.ts
@@ -1,3 +1,2 @@
 export { default as eases } from './eases'
 export { default as cubicBezier } from './cubic_bezier'
-export { default as steps } from './steps'
--- packages/kpi-site/src/motion/easing/steps.ts
+++
@@ -1,7 +0,0 @@
-import clamp from '../utils/clamp'
-
-function steps(frames = 10) {
-  return (t: number) => Math.ceil(clamp(t, 0.000001, 1) * frames) * (1 / frames)
-}
-
-export default steps
