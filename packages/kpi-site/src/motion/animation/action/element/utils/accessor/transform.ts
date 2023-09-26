2de711f0d76e9ca928baa5f51a52cf6fd98f6303
clearink <1561939638@qq.com>
2023-09-26 14:45:23 +0800

    123

--- packages/kpi-site/src/motion/animation/action/element/utils/accessor/transform.ts
+++ packages/kpi-site/src/motion/animation/action/element/utils/accessor/transform.ts
@@ -29,60 +29,75 @@ export const motionTransformProps = Object.freeze({
 })
 
 // 弧度变角度
-const PI_2 = Math.PI * 2
+const PI_HALF = Math.PI / 2
 const RAD_TO_DEG = 180 / Math.PI
 
-//  解析 matrix
-function parseTransformMatrix(input: string) {
+// 分解 matrix
+function decomposeMartix(input: string) {
   const matched = parseFunctionString(input)
 
   const isMatrix3d = matched && matched.name === 'matrix3d'
 
-  const matrix = matched ? matched.args.map(Number) : [1, 0, 0, 1, 0, 0]
+  const matrix = matched ? matched.args.map(parseFloat) : [1, 0, 0, 1, 0, 0]
 
-  if (isMatrix3d) {
-  } else {
-    // matrix2d
-    const [a, b, c, d, x, y] = matrix
+  if (isMatrix3d) return decomposeMatrix3d(matrix)
 
-    let scaleX = Math.sqrt(a * a + b * b)
-    let scaleY = Math.sqrt(c * c + d * d)
+  return decomposeMatrix2d(matrix)
+}
 
-    let skewX = -Math.atan2(-c, d)
-    let skewY = Math.atan2(b, a)
+// matrix2d QR
+function decomposeMatrix2d(matrix: number[]) {
+  const [a, b, c, d, x, y] = matrix
 
-    const delta = Math.abs(skewX + skewY)
+  const delta = a * d - b * c
 
-    let rotate = 0
-    if (asEqual(delta, 0) || asEqual(Math.abs(Math.PI * 2 - delta), 0)) {
-      rotate = skewY * RAD_TO_DEG
-      skewX = 0
-      skewY = 0
-    } else {
-      rotate = 0
-      skewX *= RAD_TO_DEG
-      skewY *= RAD_TO_DEG
-    }
-    if (skewX) scaleY *= Math.abs(Math.cos(skewX * RAD_TO_DEG))
+  let rotate = 0
+  let scaleX = 0
+  let scaleY = 0
+  let skewX = 0
 
-    rotate = sanitize(rotate)
-    skewX = sanitize(skewX)
-    skewY = sanitize(skewY)
-    scaleX = sanitize(scaleX)
-    scaleY = sanitize(scaleY)
-
-    let result = ''
-
-    if (x || y) result += `translate3d(${x}px, ${y}px, 0) `
-
-    if (rotate) result += `rotate(${rotate}deg) `
-
-    if (scaleX || scaleY) result += `scale(${scaleX}, ${scaleY}) `
-
-    if (skewX || skewY) result += `skew(${skewX}deg, ${skewY}deg) `
-
-    console.log(result)
+  if (a || b) {
+    const r = Math.sqrt(a * a + b * b)
+    rotate = Math.acos(a / r) * (b > 0 ? 1 : -1)
+    scaleX = r
+    scaleY = delta / r
+    skewX = Math.atan((a * c + b * d) / (r * r))
+  } else if (c || d) {
+    const s = Math.sqrt(c * c + d * d)
+    rotate = PI_HALF - (d > 0 ? Math.acos(-c / s) : -Math.acos(c / s))
+    scaleX = delta / s
+    scaleY = s
+    skewX = Math.atan((a * c + b * d) / (s * s))
   }
+
+  rotate = sanitize(rotate * RAD_TO_DEG)
+  scaleX = sanitize(scaleX)
+  scaleY = sanitize(scaleY)
+  skewX = sanitize(skewX * RAD_TO_DEG)
+
+  let result = ''
+
+  if (x || y) result += `translate3d(${x}px, ${y}px, 0) `
+
+  if (rotate) result += `rotate(${rotate}deg) `
+
+  if (scaleX !== 1 || scaleY !== 1) result += `scale(${scaleX}, ${scaleY}) `
+
+  if (skewX) result += `skew(${skewX}deg)`
+
+  return result
+}
+
+function decomposeMatrix3d(matrix: number[]) {
+  const x = matrix[12]
+  const y = matrix[13]
+  const z = matrix[14]
+
+  let result = ''
+
+  if (x || y || z) result += `translate3d(${x}px, ${y}px, ${z}px) `
+
+  return result
 }
 
 export default function makeTransformAccessor(element: HTMLElement, property: string) {
@@ -127,25 +142,3 @@ function renderTransformStyle(cache: Partial<Record<string, string>>) {
 export function isElementTransform(property: string) {
   return hasOwn(motionTransformProps, property)
 }
-
-;`
-javascript function parseMatrix(matrix) { 
-  // 去掉matrix()函数和空格，只保留参数 
-  matrix = matrix.replace(/^matrix\(/i, '').replace(/\)$/, ''); 
-  // 将参数拆分成数组 
-  var matrixArray = matrix.split(','); 
-  // 获取矩阵的各个值 
-  var a = parseFloat(matrixArray[0]); 
-  var b = parseFloat(matrixArray[1]); 
-  var c = parseFloat(matrixArray[2]); 
-  var d = parseFloat(matrixArray[3]); 
-  var e = parseFloat(matrixArray[4]); 
-  var f = parseFloat(matrixArray[5]); 
-  // 计算平移、缩放、旋转和倾斜的值 
-  var translateX = e; 
-  var translateY = f; 
-  var scaleX = Math.sqrt(a * a + b * b); 
-  var scaleY = Math.sqrt(c * c + d * d); 
-  var rotate = Math.atan2(b, a) * (180 / Math.PI); 
-  var skewX = Math.atan2(-c, d) * (180 / Math.PI); 
-  var skewY = Math.atan2(b, a) * (180 / Math.PI); // 返回解析后的属性 return { translateX: translateX, translateY: translateY, scaleX: scaleX, scaleY: scaleY, rotate: rotate, skewX: skewX, skewY: skewY }; } // 示例用法 var matrix = 'matrix(0.707107, 0.707107, -0.707107, 0.707107, 0, 0)'; var parsedMatrix = parseMatrix(matrix); console.log(parsedMatrix.translateX); // 输出: 0 console.log(parsedMatrix.translateY); // 输出: 0 console.log(parsedMatrix.scaleX); // 输出: 1 console.log(parsedMatrix.scaleY); // 输出: 1 console.log(parsedMatrix.rotate); // 输出: 45 console.log(parsedMatrix.skewX); // 输出: -45 console.log(parsedMatrix.skewY); // 输出: 45 `
