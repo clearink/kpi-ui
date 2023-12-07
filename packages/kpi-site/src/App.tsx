8a52d8309cb0f57997936dc7c727824b3dc5e855
clearink <1561939638@qq.com>
2023-12-08 00:24:21 +0800

    123

--- packages/kpi-site/src/App.tsx
+++ packages/kpi-site/src/App.tsx
@@ -1,12 +1,9 @@
 import { Button } from '@kpi-ui/components'
-import { useState } from 'react'
 
 import '@kpi-ui/components/src/style'
 import './style.scss'
 
 export default function App() {
-  const [cur, set] = useState(-1)
-
   return (
     <div className="window">
       <div>
